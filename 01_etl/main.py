977507c248305ed73defdd30c1bf8950907ccb08
Damir Kambarov <damir.kambarov@qualixdev.com>
2022-05-10 13:42:23 +0500

    transformer refactored

--- 01_etl/main.py
+++ 01_etl/main.py
@@ -8,7 +8,7 @@ import psycopg2
 from config import index_name, ESSettings, PostgresSettings
 from utils import backoff, get_last_update
 from loader import create_index
-from transformer import create_data_for_elastic
+from transformer import Transformer
 from extractor import PostgresExtractor
 
 logger = logging.getLogger(__name__)
@@ -17,20 +17,21 @@ logger = logging.getLogger(__name__)
 def main():
     es_conn, pg_conn = connect()
     extractor = PostgresExtractor(pg_conn)
+    transformer = Transformer()
 
     if not es_conn.indices.exists(index=index_name):
         create_index(es_conn)
         initial_data = extractor.get_data()
-        entries = create_data_for_elastic(initial_data)
+        entries = transformer.compile_data(initial_data)
 
         return es_conn.bulk(index=index_name, body=entries)
 
-    now_plus_5_min = datetime.datetime.now() + datetime.timedelta(minutes=5)
+    now = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
     last_updated_time = get_last_update()
+    updated_data = extractor.get_data(last_updated_time)
 
-    if now_plus_5_min.strftime("%Y-%m-%d %H:%M:%S") > last_updated_time:
-        updated_data = extractor.get_data(last_updated_time)
-        entries = create_data_for_elastic(updated_data)
+    if now > last_updated_time and updated_data:
+        entries = transformer.compile_data(updated_data)
 
         return es_conn.bulk(index=index_name, body=entries)
 
--- 01_etl/transformer.py
+++ 01_etl/transformer.py
@@ -2,30 +2,30 @@ from config import index_name
 from models import Movie
 
 
-def prepare_entries(data):
-    entries = list()
-    for idx, rating, genre, title, descr, director, actors_n, writers_n, actors, writers in data:
-        entry = Movie(
-            id=idx, imdb_rating=rating, genre=genre, title=title,
-            description=descr, director=director, actors_names=actors_n,
-            writers_names=writers_n, actors=actors, writers=writers)
-        entries.append(entry)
-    return entries
+class Transformer:
+    def prepare_entries(self, data):
+        entries = list()
+        for idx, rating, genre, title, descr, director, actors_n, writers_n, actors, writers in data:
+            entry = Movie(
+                id=idx, imdb_rating=rating, genre=genre, title=title,
+                description=descr, director=director, actors_names=actors_n,
+                writers_names=writers_n, actors=actors, writers=writers)
+            entries.append(entry)
+        return entries
 
+    def compile_data(self, data):
+        entries = self.prepare_entries(data)
 
-def create_data_for_elastic(data):
-    entries = prepare_entries(data)
+        out = list()
+        for entry in entries:
+            index_template = {"index": {"_index": index_name, "_id": str(entry.id)}}
+            data_template = {
+                "id": str(entry.id), "imdb_rating": entry.imdb_rating,
+                "genre": entry.genre, "title": entry.title,
+                "description": entry.description, "director": entry.director,
+                "actors_names": entry.actors_names, "writers": entry.writers,
+                "writers_names": entry.writers_names, "actors": entry.actors,}
+            out.append(index_template)
+            out.append(data_template)
 
-    out = list()
-    for entry in entries:
-        index_template = {"index": {"_index": index_name, "_id": str(entry.id)}}
-        data_template = {
-            "id": str(entry.id), "imdb_rating": entry.imdb_rating,
-            "genre": entry.genre, "title": entry.title,
-            "description": entry.description, "director": entry.director,
-            "actors_names": entry.actors_names, "writers": entry.writers,
-            "writers_names": entry.writers_names, "actors": entry.actors,}
-        out.append(index_template)
-        out.append(data_template)
-
-    return out
+        return out
