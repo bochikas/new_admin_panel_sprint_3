175093af389fcab510af1129a969224d0fb1dada
Damir Kambarov <damir.kambarov@qualixdev.com>
2022-05-10 08:34:27 +0500

    queries update

--- 01_etl/sql_queries.py
+++ 01_etl/sql_queries.py
@@ -1,49 +1,49 @@
 last_update_query = """
-        SELECT film_work.id,
-        film_work.rating AS imdb_rating,
-        ARRAY_AGG(DISTINCT genre.name) AS genre,
-        film_work.title,
-        film_work.description,
-        ARRAY_AGG(DISTINCT person.full_name)
-        FILTER(WHERE person_film_work.role = 'director') AS director,
-        ARRAY_AGG(DISTINCT person.full_name)
-        FILTER(WHERE person_film_work.role = 'actor') AS actors_names,
-        ARRAY_AGG(DISTINCT person.full_name)
-        FILTER(WHERE person_film_work.role = 'writer') AS writers_names,
-        JSON_AGG(DISTINCT jsonb_build_object('id', person.id, 'name', person.full_name))
-        FILTER(WHERE person_film_work.role = 'actor') AS actors,
-        JSON_AGG(DISTINCT jsonb_build_object('id', person.id, 'name', person.full_name))
-        FILTER(WHERE person_film_work.role = 'writer') AS writers,
-        film_work.updated_at
-        FROM film_work
-        LEFT OUTER JOIN genre_film_work ON (film_work.id = genre_film_work.film_work_id)
-        LEFT OUTER JOIN genre ON (genre_film_work.genre_id = genre.id)
-        LEFT OUTER JOIN person_film_work ON (film_work.id = person_film_work.film_work_id)
-        LEFT OUTER JOIN person ON (person_film_work.person_id = person.id)
-        WHERE film_work.updated_at > '%s'
-        GROUP BY film_work.id, film_work.title, film_work.description, film_work.rating
+        SELECT fw.id,
+        fw.rating AS imdb_rating,
+        ARRAY_AGG(DISTINCT g.name) AS genre,
+        fw.title,
+        fw.description,
+        ARRAY_AGG(DISTINCT p.full_name)
+        FILTER(WHERE pfw.role = 'director') AS director,
+        ARRAY_AGG(DISTINCT p.full_name)
+        FILTER(WHERE pfw.role = 'actor') AS actors_names,
+        ARRAY_AGG(DISTINCT p.full_name)
+        FILTER(WHERE pfw.role = 'writer') AS writers_names,
+        JSON_AGG(DISTINCT jsonb_build_object('id', p.id, 'name', p.full_name))
+        FILTER(WHERE pfw.role = 'actor') AS actors,
+        JSON_AGG(DISTINCT jsonb_build_object('id', p.id, 'name', p.full_name))
+        FILTER(WHERE pfw.role = 'writer') AS writers,
+        fw.updated_at
+        FROM film_work fw
+        LEFT OUTER JOIN genre_film_work gfw ON (fw.id = gfw.film_work_id)
+        LEFT OUTER JOIN genre g ON (gfw.genre_id = g.id)
+        LEFT OUTER JOIN person_film_work pfw ON (fw.id = pfw.film_work_id)
+        LEFT OUTER JOIN person p ON (pfw.person_id = p.id)
+        WHERE fw.updated_at > '%s'
+        GROUP BY fw.id, fw.title, fw.description, fw.rating
         """
 
 all_data_query = """
-        SELECT film_work.id,
-        film_work.rating AS imdb_rating,
-        ARRAY_AGG(DISTINCT genre.name) AS genre,
-        film_work.title,
-        film_work.description,
-        ARRAY_AGG(DISTINCT person.full_name)
-        FILTER(WHERE person_film_work.role = 'director') AS director,
-        ARRAY_AGG(DISTINCT person.full_name)
-        FILTER(WHERE person_film_work.role = 'actor') AS actors_names,
-        ARRAY_AGG(DISTINCT person.full_name)
-        FILTER(WHERE person_film_work.role = 'writer') AS writers_names,
-        JSON_AGG(DISTINCT jsonb_build_object('id', person.id, 'name', person.full_name))
-        FILTER(WHERE person_film_work.role = 'actor') AS actors,
-        JSON_AGG(DISTINCT jsonb_build_object('id', person.id, 'name', person.full_name))
-        FILTER(WHERE person_film_work.role = 'writer') AS writers
-        FROM film_work
-        LEFT OUTER JOIN genre_film_work ON (film_work.id = genre_film_work.film_work_id)
-        LEFT OUTER JOIN genre ON (genre_film_work.genre_id = genre.id)
-        LEFT OUTER JOIN person_film_work ON (film_work.id = person_film_work.film_work_id)
-        LEFT OUTER JOIN person ON (person_film_work.person_id = person.id)
-        GROUP BY film_work.id, film_work.title, film_work.description, film_work.rating
+        SELECT fw.id,
+        fw.rating AS imdb_rating,
+        ARRAY_AGG(DISTINCT g.name) AS genre,
+        fw.title,
+        fw.description,
+        ARRAY_AGG(DISTINCT p.full_name)
+        FILTER(WHERE pfw.role = 'director') AS director,
+        ARRAY_AGG(DISTINCT p.full_name)
+        FILTER(WHERE pfw.role = 'actor') AS actors_names,
+        ARRAY_AGG(DISTINCT p.full_name)
+        FILTER(WHERE pfw.role = 'writer') AS writers_names,
+        JSON_AGG(DISTINCT jsonb_build_object('id', p.id, 'name', p.full_name))
+        FILTER(WHERE pfw.role = 'actor') AS actors,
+        JSON_AGG(DISTINCT jsonb_build_object('id', p.id, 'name', p.full_name))
+        FILTER(WHERE pfw.role = 'writer') AS writers
+        FROM film_work fw
+        LEFT OUTER JOIN genre_film_work gfw ON (fw.id = gfw.film_work_id)
+        LEFT OUTER JOIN genre g ON (gfw.genre_id = g.id)
+        LEFT OUTER JOIN person_film_work pfw ON (fw.id = pfw.film_work_id)
+        LEFT OUTER JOIN person p ON (pfw.person_id = p.id)
+        GROUP BY fw.id, fw.title, fw.description, fw.rating
         """
